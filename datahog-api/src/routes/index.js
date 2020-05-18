0761dfe1e205316fbf9bdfbd5b86dfd8998d56fd
prasanjitmohanty <prasanjit.class@gmail.com>
2020-05-18 22:55:51 +0100

    req validation

--- datahog-api/src/routes/index.js
+++ datahog-api/src/routes/index.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const REQUEST_MAIN_EXCHANGE =  'datahog.exchange.main';
+const REQUEST_MAIN_EXCHANGE = 'datahog.exchange.main';
 const messagePublisher = require('../message-broker/message-publisher')(REQUEST_MAIN_EXCHANGE);
 
 routes.get('/', (req, res) => {
@@ -7,17 +7,21 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/health', function (req, res) {
-    res.send('datahog up...');
-  });
-  
-  routes.post('/processProvider', function (req, res) {
-    console.log(JSON.stringify(req.body));
-    let provider = req.body.provider;
-    let callbackUrl = req.body.callbackUrl;
+  res.send('datahog up...');
+});
+
+routes.post('/processProvider', function (req, res) {
+  console.log(JSON.stringify(req.body));
+  let provider = req.body.provider;
+  let callbackUrl = req.body.callbackUrl;
+  if (provider && callbackUrl) {
     //Queue the request for processing
     messagePublisher.publish(req.body);
-  
-    res.send(`Got a POST request ${provider}:${callbackUrl}`);
-  });
+  } else {
+    throw new Error(`Invalid request`);
+  }
+
+  res.send(`Got a POST request ${provider}:${callbackUrl}`);
+});
 
 module.exports = routes;
